dd66340d29bcab5995057e7f3c1cc452f08b6c5f
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-16 19:29:06 +0200

    Add files via upload

---
+++ Foods_add_new_item_inlist.py
@@ -0,0 +1,14 @@
+my_foods = ["pizza", "pasta", "pasta", "fruits"]
+friends_foods = my_foods[:]
+
+my_foods.append("burgers")
+friends_foods.append("fish")
+
+
+print("My favorite foods are :")
+print(my_foods)
+
+print("\nMy friend's favorite food are:")
+print(friends_foods)
+
+#copying a list from a slice and add new item in list
