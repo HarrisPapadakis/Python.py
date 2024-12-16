c40a8ca20ad2c2d7260e692d02bb62190f62c865
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-16 14:16:31 +0200

    Add files via upload

---
+++ Foods.py
@@ -0,0 +1,8 @@
+my_foods = ["pizza", "pasta", "pasta", "fruits"]
+friends_foods = my_foods[:]
+
+print("My favorite foods are :")
+print(my_foods)
+
+print("\nMy friend's favorite food are:")
+print(friends_foods)
