42815dfd12dce4c511ca0d292e6a7a3f20840dcb
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-19 12:57:27 +0200

    Add files via upload

---
+++ voting_right.py
@@ -0,0 +1,8 @@
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+else:
+    print("You are too young to vote")
+    print("Please register to vote as soon your turn 18!")
