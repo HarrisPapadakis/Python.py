832ade4c0d2edb0f8a0a580a34a7f0b1d20fefda
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-20 17:24:52 +0200

    Add files via upload

---
+++ alien_colour.py
@@ -0,0 +1,8 @@
+alien_colour = "green"
+
+if alien_colour == "green":
+    print("You just won 5 points!")
+elif alien_colour == "red":
+    print("You just won 5 points!")
+elif alien_colour == "yellow":
+    print("You just won 5 points!")
---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+alien_colour = "red" #Using if/elif/else
+
+if alien_colour == "green":
+    print("You just won 5 points cause you shot the alien!")
+elif alien_colour != "green":
+    print("You just won 10 points!")
+else:
+    print("No action happened!")
