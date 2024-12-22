182b326106a58658e65127cb679bf49d4afc13d9
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-22 18:47:38 +0200

    Add files via upload

---
+++ alien.py
@@ -0,0 +1,8 @@
+# Define a dictionary with keys "color" and "points"
+alien_0 = {"color": "green", "points": 5}
+
+# Access and print the value of the "color" key
+print(alien_0["color"])  # Output: green
+
+# Access and print the value of the "points" key
+print(alien_0["points"])  # Output: 5
