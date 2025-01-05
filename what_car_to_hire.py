b810dd6d67e52c52411259355c47b76554e1979a
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-01-06 01:25:57 +0200

    Add files via upload

---
+++ what_car_to_hire.py
@@ -0,0 +1,5 @@
+# Prompt the user to input the type of car they would like to rent and store the response in the variable `car`
+car = input("What car would you like to rent? ")
+
+# Use an f-string to print a message indicating that you are checking the availability of the requested car
+print(f"Let me check if I can find you a {car}.")
