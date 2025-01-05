0d7e1d7e397d91aadb65e434dcdabf94a49f2399
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-01-05 16:55:40 +0200

    Add files via upload

---
+++ greeter.py
@@ -0,0 +1,5 @@
+# Ask the user to enter their name and store the input in the variable `name`
+name = input("Please enter your name: ") 
+
+# Use an f-string to display a personalized greeting message
+print(f"\nHello, {name}!") 
