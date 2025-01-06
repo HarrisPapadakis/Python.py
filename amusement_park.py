7b727d5fe6d34ca1b6c3ace5e1b0c27427b1566d
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-01-06 11:07:03 +0200

    Update amusement_park.py

--- amusement_park.py
+++ amusement_park.py
@@ -1,8 +1,15 @@
+# Define the variable `age` and assign it the value 12
 age = 12
 
+# Check if the age is less than 4
 if age < 4:
+    # If the condition is true, print that the admission cost is $0
     print("Your admission cost is $0.")
+# Check if the age is less than 18 (and greater than or equal to 4 due to previous condition)
 elif age < 18:
+    # If the condition is true, print that the admission cost is $25
     print("Your admission cost is $25.")
+# If none of the above conditions are true, execute the following
 else:
+    # Print that the admission cost is $40 for ages 18 or older
     print("Your admission cost is $40.")
