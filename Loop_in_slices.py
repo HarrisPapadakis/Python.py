c1b4a29632c34861147183067ae07ecc558d49e5
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-02-08 15:40:36 +0200

    Update Loop_in_slices.py

--- Loop_in_slices.py
+++ Loop_in_slices.py
@@ -1,6 +1,10 @@
+# Define a list of players on the team
 players = ['ronaldinho', 'messi', 'cristiano ronaldo', 'kross', 'sergio ramos', 'pineda']
 
+# Display a message indicating the first three players will be shown
 print("Here are the first three players on my team:")
-for player in players[0:3]:
-    print(player.title())
 
+# Loop through the first three players in the list
+for player in players[0:3]:
+    # Print each player's name with the first letter of each word capitalized
+    print(player.title())
