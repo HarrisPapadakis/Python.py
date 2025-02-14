9e5997121f63cc5cc4bf57a6277137db1fe9b3e9
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-02-14 23:02:20 +0200

    Update alien_colour.py

--- alien_colour.py
+++ alien_colour.py
@@ -1,8 +1,9 @@
 alien_colour = "green"
 
+# Check the color of the alien and assign points accordingly
 if alien_colour == "green":
-    print("You just won 5 points!")
+    print("You just won 5 points!")  # Award 5 points for green alien
 elif alien_colour == "red":
-    print("You just won 5 points!")
+    print("You just won 5 points!")  # Award 5 points for red alien
 elif alien_colour == "yellow":
-    print("You just won 5 points!")
+    print("You just won 5 points!")  # Award 5 points for yellow alien
