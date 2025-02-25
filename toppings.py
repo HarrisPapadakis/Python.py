8ebc533a6dd434571b1901c73fdadcd90d998be5
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-02-25 19:47:59 +0200

    Update toppings.py

--- toppings.py
+++ toppings.py
@@ -1,10 +1,17 @@
+# List of requested toppings for the pizza
 requested_toppings = ["mushrooms", "extra cheese"]
 
+# Check if "mushrooms" is in the list and print a message if it is
 if "mushrooms" in requested_toppings:
     print("Adding mushrooms.")
+
+# Check if "pepperoni" is in the list and print a message if it is
 if "pepperoni" in requested_toppings:
     print("Adding pepperoni.")
+
+# Check if "extra cheese" is in the list and print a message if it is
 if "extra cheese" in requested_toppings:
     print("Adding extra cheese ")
 
+# Print a final message after adding all toppings
 print("\nFinished making your pizza!")
