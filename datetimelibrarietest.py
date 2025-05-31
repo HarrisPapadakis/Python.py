f0067c5a2ff7bf4ebbfa2abf4d39f86e8f5b662f
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-05-31 21:00:47 +0300

    Update datetimelibrarietest.py

--- datetimelibrarietest.py
+++ datetimelibrarietest.py
@@ -3,6 +3,6 @@ import datetime
 # Δημιουργία συγκεκριμένης ημερομηνίας 
 x = datetime.datetime(2023, 1, 1)
 
-print(x.strftime("%A"))   # Full weekday name -> "Saturday"
-print(x.strftime("%Y"))   # Year -> "2021"
-print(x.strftime("%d/%m/%Y"))  # Formatted date -> "25/12/2021"
+print(x.strftime("%A"))   # Εκτυπώνουμε το όνομα της ημέρας
+print(x.strftime("%Y"))   # Εκτυπώνουμε μόνο το έτος
+print(x.strftime("%d/%m/%Y"))  # Εκτυπώνουμε την ημερομηνία σε μορφή: Ημέρα/Μήνας/Έτος
